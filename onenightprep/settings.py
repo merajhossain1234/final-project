7c64dd9e2d57524d44843326084634484959c6d3
SouravBiswas238 <souravbiswas427@gmail.com>
2025-07-09 11:40:05 +0600

    update

--- onenightprep/settings.py
+++ onenightprep/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',  # Add this
     'accounts',
     'ai',
     'session',
